d2bd69d3ed9764380a52a60725121e4257e06d80
monte777 <montenegromasis@hotmail.com>
2019-11-03 02:56:38 -0600

    actualización tabla 1

--- Codigo/01_extraccion_pdf.R
+++ Codigo/01_extraccion_pdf.R
@@ -5,20 +5,20 @@ library(pdftools)
 library(readr)    
 library(data.table)
 library(stringr)
+library(RDS)
 
 
 file_vector <- list.files(path = "PDF/")
 file_vector %>% head()
 
-a<- pdf_text(paste0("PDF/",file_vector[[2]])) %>% 
+pdf_text(paste0("PDF/",file_vector[[2]])) %>% 
     strsplit(split = "\n")
 
 
 tablas <- vector("list", NROW(file_vector))
-names(tablas) <- file_vector
 
 for (i in seq_along(file_vector)) {
-    leer <- pdf_text(paste0("PDF/",file_vector[[3]])) %>% 
+    leer <- pdf_text(paste0("PDF/",file_vector[[i]])) %>% 
         strsplit(split = "\n")
     validos <-grepl("[12]\\d{3}\\s{5}",leer[[1]])
     
@@ -26,5 +26,11 @@ for (i in seq_along(file_vector)) {
     b<- str_trim(a)
     c<- strsplit(b,"[[:blank:]]")
     c2<-lapply(c, function(x) x[!x %in% ""])
-    c3 = rbindlist(lapply(c, function(x) data.table((t(x)))),fill = TRUE)
+    c3 <- rbindlist(lapply(c2, function(x) data.table((t(x)))),fill = TRUE) %>% 
+        dplyr::select(V1:V15) %>% mutate(archivo= !!file_vector[[i]])
+    tablas[[i]] <- c3
 }
+
+datos <- bind_rows(tablas)  
+
+save(datos, file = "Datos/datos_tabla1.Rds")
