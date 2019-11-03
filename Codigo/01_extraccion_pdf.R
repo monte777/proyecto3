316c8a9ab108ea4d267b1b774e6a8b694c3ba55e
monte777 <montenegromasis@hotmail.com>
2019-11-03 02:21:35 -0600

    act

--- Codigo/01_extraccion_pdf.R
+++ Codigo/01_extraccion_pdf.R
@@ -2,11 +2,15 @@
 
 library(dplyr)
 library(pdftools)
+library(readr)    
+library(data.table)
+library(stringr)
+
 
 file_vector <- list.files(path = "PDF/")
 file_vector %>% head()
 
-pdf_text("PDF/69512_Zarcero.pdf") %>% 
+a<- pdf_text(paste0("PDF/",file_vector[[2]])) %>% 
     strsplit(split = "\n")
 
 
@@ -14,6 +18,13 @@ tablas <- vector("list", NROW(file_vector))
 names(tablas) <- file_vector
 
 for (i in seq_along(file_vector)) {
-    primera_linea <- read_lines(file_vector[i], n_max = 1)
+    leer <- pdf_text(paste0("PDF/",file_vector[[3]])) %>% 
+        strsplit(split = "\n")
+    validos <-grepl("[12]\\d{3}\\s{5}",leer[[1]])
     
+    a<-paste(leer[[1]][validos])
+    b<- str_trim(a)
+    c<- strsplit(b,"[[:blank:]]")
+    c2<-lapply(c, function(x) x[!x %in% ""])
+    c3 = rbindlist(lapply(c, function(x) data.table((t(x)))),fill = TRUE)
 }
