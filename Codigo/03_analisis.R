a3949a995198bbcb607a5a23ec304ca865f955fb
Pablex01 <44558197+Pablex01@users.noreply.github.com>
2019-11-04 11:21:13 -0600

    Act

--- Codigo/03_analisis.R
+++ Codigo/03_analisis.R
@@ -1,10 +1,13 @@
 #Cargamos la librerías que se utilizarán
 
-library(tidyverse)
 library(gstat)
 library(sf)
+library(sp)
 library(MASS)
 library(dismo)
+library(tmap)
+library(rgdal)
+library(tidyverse)
 
 #cargamos los datos
 datos <- readRDS("Datos/datos_finales.Rds")
@@ -16,13 +19,13 @@ colnames(datos) <- tolower(colnames(datos))
 #Media de temperatura
 datos1 <- datos %>% 
     filter(elementos == "LLUVIA") %>%
-    select(cod_estacion,lat,lon,alt,ene,feb,mar,abr,may,jun,jul,ago,set,oct,nov,dic) %>% 
+    dplyr::select(cod_estacion,lat,lon,alt,ene,feb,mar,abr,may,jun,jul,ago,set,oct,nov,dic) %>% 
     mutate_if(is.character,as.numeric) %>% 
     mutate(cod = factor(cod_estacion)) %>% 
     group_by(cod) %>% 
     mutate(lluviaprom = mean(c(ene,feb,mar,abr,may,jun,jul,ago,set,oct,nov,dic))) %>% 
-    select(cod,lat,lon,alt,lluviaprom)
+    dplyr::select(cod,lat,lon,alt,lluviaprom)
 
-rm(datos)
+datos_sp <- st_as_sf(datos1,coords = c("lon","lat")) 
 
-
+rm(datos,datos1)
